51dd88c575671cf069aec6ef7e5a3cece190d750
GODston <gastontejedav@gmail.com>
2023-03-28 22:50:14 -0600

    nuget Xamarin.CommunityToolkit

--- Spots/Spots/Spots/Views/vwHomePage/vwHomePage.xaml.cs
+++ Spots/Spots/Spots/Views/vwHomePage/vwHomePage.xaml.cs
@@ -10,28 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace Spots.Views.vwHomePage
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class vwHomePage : FlyoutPage
+    public partial class vwHomePage : ContentPage
     {
         public vwHomePage()
         {
             InitializeComponent();
-            FlyoutPage.ListView.ItemSelected += ListView_ItemSelected;
-            NavigationPage.SetHasNavigationBar(this, false);
-        }
-
-        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            var item = e.SelectedItem as vwHomePageFlyoutMenuItem;
-            if (item == null)
-                return;
-
-            var page = (Page)Activator.CreateInstance(item.TargetType);
-            page.Title = item.Title;
-
-            Detail = new NavigationPage(page);
-            IsPresented = false;
-
-            FlyoutPage.ListView.SelectedItem = null;
         }
     }
 }
