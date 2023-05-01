5ad2b770315cf9fe9c1f05e59b485bcb48be26b6
GODston <gastontejedav@gmail.com>
2023-05-01 16:57:18 -0600

    Added Launch of Firebase Auth

--- Spots/Spots/MauiProgram.cs
+++ Spots/Spots/MauiProgram.cs
@@ -1,4 +1,13 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Bundled.Shared;
+
+#if IOS
+	using Plugin.Firebase.Core.Platforms.iOS;
+#else
+	using Plugin.Firebase.Core.Platforms.Android;
+#endif
 
 namespace Spots;
 
@@ -9,6 +18,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.RegisterFirebaseServices()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -21,4 +31,26 @@ public static class MauiProgram
 
 		return builder.Build();
 	}
+
+    private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+    {
+        builder.ConfigureLifecycleEvents(events => {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((_,__) => {
+                CrossFirebase.Initialize();
+				FirebaseAuthImplementation.Initialize();
+                return false;
+            }));
+#else
+            events.AddAndroid(android => android.OnCreate((activity, _) =>
+            {
+                CrossFirebase.Initialize(activity);
+                FirebaseAuthImplementation.Initialize("443931860533-lvbmdbnge1tdc2dmqpvmqg511ag25cv5.apps.googleusercontent.com");
+            }));
+#endif
+        });
+
+        builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+        return builder;
+    }
 }
